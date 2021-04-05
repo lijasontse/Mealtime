dca6be2b67413ba5f486ead75eb4604cf995ee35
Jason Li <li.jasontse@gmail.com>
2021-04-05 15:23:09 -0700

    Initial commit for mealtime, a Yelp clone

---
+++ app/models/user.rb
@@ -0,0 +1,47 @@
+class User < ApplicationRecord
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, :last_name, presence: true 
+  validates :password, 
+    length: { minimum: 6, 
+              message: 'Password must be at least 6 characters in length'
+              allow_nil: true }
+  validates :session_token, presence: true, uniquenss: true
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && user.is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  private
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+
+
+end
