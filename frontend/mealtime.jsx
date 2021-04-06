845560882fb32c338deb03be64f39ad17bdc56ef
Jason Li <li.jasontse@gmail.com>
2021-04-06 15:41:41 -0700

    fix signup button

--- frontend/mealtime.jsx
+++ frontend/mealtime.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { login, logout, signup } from './util/session_api_util';
+import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
 
