640ff3fbbd49772b8c169cfba4ac3c743e0ead07
Jason Li <li.jasontse@gmail.com>
2021-04-06 10:05:38 -0700

    keep up to date with main

--- frontend/mealtime.jsx
+++ frontend/mealtime.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 import { login, logout, signup } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
 
+  const store = configureStore();
+
   window.login = login;
   window.signup = signup;
   window.logout = logout; 
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to MealTime!</h1>, root);
