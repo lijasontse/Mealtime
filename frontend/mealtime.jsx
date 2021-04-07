4c295db4a872bbe6b3fc051f204eacb047cd6c3d
Jason Li <li.jasontse@gmail.com>
2021-04-06 18:16:02 -0700

    add preloadedState to entry file

--- frontend/mealtime.jsx
+++ frontend/mealtime.jsx
@@ -5,16 +5,27 @@ import Root from './components/root';
 import { login, logout, signup } from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-  const store = configureStore();
-
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout; 
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  let store;
+  if (window.currentUser) {
+    const preloadedState = {
+      entities: {
+        users: { [window.currentUser.id]: window.currentUser }
+      },
+      session: { id: window.currentUser.id }
+    };
+    store = configureStore();
+    delete window.currentUser;
+  } else {
+    store = configureStore();
+  }
   
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store} />, root);
 });
 
+// tests
+// window.login = login;
+// window.signup = signup;
+// window.logout = logout; 
+// window.getState = store.getState;
+// window.dispatch = store.dispatch;
