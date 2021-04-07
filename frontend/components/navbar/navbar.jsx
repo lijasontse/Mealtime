ac555455679493b2a2b0b8cbe3ec1cd44717f0b7
Jason Li <li.jasontse@gmail.com>
2021-04-07 08:59:11 -0700

    rewrite navbar.jsx code into Class Component from functional

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -1,33 +1,73 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBar = ({ currentUser, logout, location }) => {
-  const sessionLinks = () => {
-    if (location.pathname === "/login" || location.pathname === '/signup') {
-      return null;
-    } else {
-      return (
-        <nav className="nav-links">
-          <Link to="/signup" className="signup">Sign Up</Link>
-          &nbsp; &nbsp;
-          <Link to="/login" className="login">Log In</Link>
-        </nav>
-      );
-    }
-  };
+// const NavBar = ({ currentUser, logout, location }) => {
+//   const sessionLinks = () => {
+//     if (location.pathname === "/login" || location.pathname === '/signup') {
+//       return null;
+//     } else {
+//       return (
+//         <nav className="nav-links">
+//           <Link to="/signup" className="signup">Sign Up</Link>
+//           &nbsp; &nbsp;
+//           <Link to="/login" className="login">Log In</Link>
+//           <Link to="/"><img className="mt-logo" src={window.mealTimeLogo} /></Link>
+//           <img className="homepage-art" src={window.homePage} />
+//         </nav>
+//       );
+//     }
+//   };
 
-  const navDropDown = () => (
-    <div className="header-group">
-      <div className="user-dropdown">
-        <div className="user-info">
-          {currentUser.first_name} {currentUser.last_name}
+//   const navDropDown = () => (
+//     <div className="header-group">
+//       <div className="user-dropdown">
+//         <div className="user-info">
+//           {currentUser.first_name} {currentUser.last_name}
+//         </div>
+//         <button className="header-button" onClick={logout}>Log Out</button>
+//       </div>
+//     </div>
+//   );
+
+//   return currentUser ? navDropDown() : sessionLinks();
+// };
+
+// export default NavBar;
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { currentUser, logout } = this.props;
+    const display = currentUser ? (
+      <div className="nav-list">
+        <div className="user-dropdown">
+          <div className="user-info">
+            {currentUser.first_name} {currentUser.last_name}
+          </div>
         </div>
-        <button className="header-button" onClick={logout}>Log Out</button>
+        <button className="logout" onClick={logout}>Log Out</button>
       </div>
-    </div>
-  );
+    ) : (
+      <nav className="nav-links">
+        <Link to="/signup" className="signup">Sign Up</Link>
+        <Link to="/login" className="login">Log In</Link>
+      </nav>
+    );
 
-  return currentUser ? navDropDown() : sessionLinks();
-};
-
+    return (
+      <div className="main-nav">
+        <div className="right-nav">
+          {display}
+        </div>
+        <Link to="/"><img className="mt-logo" src={window.mealTimeLogo} /></Link>
+        <div className="homepage-img">
+          <img className="homepage-art" src={window.homePage} />
+        </div>
+      </div>
+    );
+  }
+}
 export default NavBar;
