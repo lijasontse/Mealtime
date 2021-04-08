423ff5f8db16c292b4449d43434601e0e7c177e3
Jason Li <li.jasontse@gmail.com>
2021-04-08 13:52:18 -0700

    write business controller

--- app/controllers/api/businesses_controller.rb
+++ app/controllers/api/businesses_controller.rb
@@ -1,2 +1,42 @@
 class Api::BusinessesController < ApplicationController
+
+  before_action :require_logged_in, only: [:create]
+
+  def index
+    @businesses = Business.all
+    render :index
+  end
+
+  def show
+    @business = Business.find(params[:id])
+  end
+
+  def create
+    @business = Business.new(business_params)
+    if @business.save
+      render "api/businesses/show"
+    else
+      render json: @business.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @business = Business.find_by(id: params[:id])
+    if @business.update(business_params)
+      render "/api/businesses/show"
+    else
+      render json: @business.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def business_params
+    params.require(:business).permit(
+      :name, :category, :address, :city, 
+      :state, :zip_code, :phone_number)
+  end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   namespace :api, default: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
+    resources :businesses, only: [:index, :show, :create]
   end
 end
