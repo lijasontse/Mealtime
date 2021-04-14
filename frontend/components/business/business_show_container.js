1ffb63b8b6cad3a062933f151405241164ea1a19
Jason Li <li.jasontse@gmail.com>
2021-04-14 16:01:40 -0700

    add changes to show container for reviews and users

--- frontend/components/business/business_show_container.js
+++ frontend/components/business/business_show_container.js
@@ -1,17 +1,21 @@
 import { connect } from 'react-redux';
 import { fetchBusiness } from '../../actions/business_actions';
+import { fetchUsers } from '../../actions/user_actions';
 import { fetchReviews, deleteReview } from '../../actions/review_actions';
 import BusinessShow from './business_show';
 
 const mapStateToProps = (state, ownProps) => ({
   business: state.entities.businesses[ownProps.match.params.businessId],
   reviews: Object.values(state.entities.reviews).filter(
-    (review) => review.business_id === ownProps.match.paras.id
-  )
+    (review) => review.business_id === ownProps.match.params.id
+  ),
+  users: state.entities.users
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchBusiness: businessId => dispatch(fetchBusiness(businessId))
+  fetchBusiness: businessId => dispatch(fetchBusiness(businessId)),
+  fetchReviews: () => dispatch(fetchReviews()),
+  fetchUsers: () => dispatch(fetchUsers())
 });
 
 export default connect(
